cd0fb65f9868c3c37d8523b92222764c91b967f1
nerdzgr <info@drupin.com>
2012-07-26 19:51:23 +0300

    adding licence info

--- magaziadmin.php
+++ magaziadmin.php
@@ -9,6 +9,25 @@ Version: 1.0
 Author URI: http://aristeides.com
 */
 
+/*
+ * The Magazi admin theme is a simple and clean WordPress administration theme.
+ * 
+ * Copyright (C) 2012  Aristeides Stathopoulos
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+ 
 function magazi_admin_head() {
 	echo '<link rel="stylesheet" type="text/css" href="' .plugins_url('admin.css', __FILE__). '">';
 }
